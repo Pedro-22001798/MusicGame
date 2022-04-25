dd8b7c8735b6cec34ece68570f4bebf1199bc378
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2022-04-25 13:23:22 +0100

    Score system

--- Assets/Scripts/NoteClick.cs
+++ Assets/Scripts/NoteClick.cs
@@ -14,31 +14,41 @@ public class NoteClick : MonoBehaviour
     private static bool jValue = false;
 
     private bool gColl;
+    private bool hColl;
+    private bool jColl;
 
-    private float gTimer = 10f;
+    private static int points;
+    [SerializeField] private Text pointsText;
+
+    //[SerializeField] private ParticleSystem hitParticles;
 
     void Start()
     {
         normalNote = new Color(255, 255, 255);
         pressedNote = new Color(0, 0, 0);
+        points = 0;
     }
 
     void Update()
     {
-        print(gTimer);
-        gColl = DetectColl.CheckGColl();
+        gColl = DetectCollG.CheckGColl();
+        hColl = DetectCollH.CheckHColl();
+        jColl = DetectCollJ.CheckJColl();
 
-        if (Input.GetButtonDown("g"))
+        if(points < 0)
         {
+            points = 0;
+        }
+
+        if (Input.GetButton("g"))
+        {
+            //hitParticles.Play();
             buttons[0].GetComponent<Renderer>().material.SetColor("_Color", pressedNote);
             gValue = true;
             if(gColl == true)
             {
-                Destroy(DetectColl.gNote());
-            }
-            else
-            {
-                print("Colisão error");
+                Destroy(DetectCollG.gNote());
+                ++points;
             }
         }
         else
@@ -46,26 +56,41 @@ public class NoteClick : MonoBehaviour
             buttons[0].GetComponent<Renderer>().material.SetColor("_Color", normalNote);
             gValue = false;
         }
+
         if (Input.GetButton("h"))
         {
+            //hitParticles.Play();
             buttons[1].GetComponent<Renderer>().material.SetColor("_Color", pressedNote);
             hValue = true;
+            if (hColl == true)
+            {
+                Destroy(DetectCollH.hNote());
+                ++points;
+            }
         }
         else
         {
             buttons[1].GetComponent<Renderer>().material.SetColor("_Color", normalNote);
             hValue = false;
         }
+
         if (Input.GetButton("j"))
         {
+            //hitParticles.Play();
             buttons[2].GetComponent<Renderer>().material.SetColor("_Color", pressedNote);
             jValue = true;
+            if (jColl == true)
+            {
+                Destroy(DetectCollJ.jNote());
+                ++points;
+            }
         }
         else
         {
             buttons[2].GetComponent<Renderer>().material.SetColor("_Color", normalNote);
             jValue = false;
         }
+        pointsText.text = points.ToString("0");
     }
 
     public static bool returngValue()
@@ -79,5 +104,9 @@ public class NoteClick : MonoBehaviour
     public static bool returnjValue()
     {
         return jValue;
+    }
+    public static void lossPoints()
+    {
+        --points;
     }
 }
